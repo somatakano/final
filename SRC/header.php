f52ccefe3ef37b58f8e1276eea38b22f4083701d
somatakano <148741500+somatakano@users.noreply.github.com>
2024-01-10 18:51:41 +0900

    up

--- SRC/header.php
+++ SRC/header.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="UTF-8">
         <title>酒吞みブログ管理システム</title>
+        <link rel="stylesheet" href="./css/style.css">
     </head>
     <body>
         <h1>酒吞みブログ</h1>
