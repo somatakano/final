d8a3af49ee8caf90fae2a0154620d92055f8100d
somatakano <148741500+somatakano@users.noreply.github.com>
2024-01-10 18:44:43 +0900

    Create db-connect.php

---
+++ SRC/db-connect.php
@@ -0,0 +1,10 @@
+<?php
+
+    const server = "mysql220.phy.lolipop.lan";
+    const dbname = "LAA1517464-final";
+    const user = "LAA1517464";
+    const pass = "Pass0706";
+
+    $connect = 'mysql:host='. server . ';dbname='. dbname . ';charset=utf8';
+    
+?>
